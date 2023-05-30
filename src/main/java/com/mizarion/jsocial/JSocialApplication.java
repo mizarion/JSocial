eb30bc9071efc1ec19d6b2bd5b7ba9961e4d5c50
mizarion <zsgluschenko@edu.hse.ru>
2023-05-30 14:34:08 +0300

    init

---
+++ src/main/java/com/mizarion/jsocial/JSocialApplication.java
@@ -0,0 +1,13 @@
+package com.mizarion.jsocial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JSocialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JSocialApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mizarion/jsocial/JSocialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mizarion.jsocial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JSocialApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
